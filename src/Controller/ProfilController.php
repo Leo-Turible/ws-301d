3c74bc84ca587ea1f83c02c098d422f72a088c73
Leo <leo.turible@etudiant.univ-reims.fr>
2023-11-22 16:58:05 +0100

    sign up + sign in + cours.json + users.json made

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -5,14 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class ProfilController extends AbstractController
 {
     #[Route('/profil', name: 'app_profil')]
-    public function index(): Response
+    public function index(UserInterface $user): Response
     {
+        dump($user->getRoles()); // Debugging line
+        if (!$this->isGranted('ROLE_USER')) {
+            // Redirect or handle unauthorized access
+            throw $this->createAccessDeniedException('Access Denied.');
+        }
+
         return $this->render('profil/index.html.twig', [
-            'controller_name' => 'ProfilController',
+            'user' => $user,
         ]);
     }
 }
