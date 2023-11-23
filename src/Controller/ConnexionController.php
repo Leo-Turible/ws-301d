b4a786f4e691c04dad180517fcb291c2f03612a2
Leo <leo.turible@etudiant.univ-reims.fr>
2023-11-23 09:57:51 +0100

    Login + Register + Profil pages functional

--- src/Controller/ConnexionController.php
+++ src/Controller/ConnexionController.php
@@ -9,7 +9,6 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 class ConnexionController extends AbstractController
 {
@@ -23,27 +22,20 @@ class ConnexionController extends AbstractController
     #[Route('/connexion', name: 'app_connexion')]
     public function index(Request $request, AuthenticationUtils $authenticationUtils, SessionInterface $session): Response
     {
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        // Check if the form is submitted
         if ($request->isMethod('POST')) {
-            // Get the submitted email and password
             $email = $request->request->get('_email');
             $password = $request->request->get('_password');
 
-            // Validate the credentials against users from the JSON file
             $isValid = $this->validateCredentials($email, $password);
 
             if ($isValid) {
-                // Save the email in the session
                 $session->set('user_email', $email);
 
                 $this->addFlash('success', 'Login successful!');
-                sleep(1.5); // Add a 3-second delay
+                sleep(1.5);
                 return $this->redirectToRoute('app_profil');
             } else {
                 $this->addFlash('error', 'Invalid credentials!');
@@ -58,15 +50,11 @@ class ConnexionController extends AbstractController
 
     private function validateCredentials($enteredEmail, $enteredPassword)
     {
-        // Load user data from the JSON file
         $users = $this->loadUsersFromJson();
 
-        // Validate the entered credentials against users from the JSON file
         foreach ($users as &$user) {
             if ($enteredEmail === $user['email'] && $enteredPassword === $user['password']) {
-                // $user['roles'] = ['ROLE_USER'];
                 
-                // $this->saveUsersToJson($users);
                 return true;
             }
         }
@@ -74,19 +62,9 @@ class ConnexionController extends AbstractController
         return false;
     }
 
-
     private function loadUsersFromJson()
     {
         $jsonContent = file_get_contents($this->getParameter('kernel.project_dir') . '/public/assets/json/users.json');
         return $this->serializer->decode($jsonContent, 'json');
     }
-
-    // private function saveUsersToJson($users)
-    // {
-    //     // Encoder les utilisateurs au format JSON
-    //     $jsonContent = $this->serializer->encode($users, 'json');
-
-    //     // Sauvegarder le contenu JSON dans le fichier
-    //     file_put_contents($this->getParameter('kernel.project_dir') . '/public/assets/json/users.json', $jsonContent);
-    // }
 }
--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -5,21 +5,85 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ProfilController extends AbstractController
 {
-    #[Route('/profil', name: 'app_profil')]
-    public function index(Security $security): Response
-{
-    if (!$security->isGranted('ROLE_USER')) {
-        return $this->redirectToRoute('app_connexion');
+    private $serializer;
+
+    public function __construct(SerializerInterface $serializer)
+    {
+        $this->serializer = $serializer;
     }
 
-    $user = $security->getUser();
+    #[Route('/profil', name: 'app_profil')]
+    public function index(SessionInterface $session): Response
+    {
+        $userEmail = $session->get('user_email');
 
-    return $this->render('profil/index.html.twig', [
-        'user' => $user,
-    ]);
-}
+        if ($userEmail) {
+            $user = $this->findUserByEmail($userEmail);
+
+            if ($user) {
+                return $this->render('profil/index.html.twig', [
+                    'user' => $user,
+                ]);
+            } else {
+                $this->addFlash('error', 'User not found!');
+                return $this->redirectToRoute('app_connexion');
+            }
+        } else {
+            $this->addFlash('error', 'User not authenticated!');
+            return $this->redirectToRoute('app_connexion');
+        }
+    }
+
+    private function findUserByEmail($email)
+    {
+        $users = $this->loadUsersFromJson();
+
+        foreach ($users as &$user) {
+            if ($email === $user['email']) {
+                // Vérifiez si l'utilisateur a les clés nécessaires
+                $requiredKeys = ['email'];
+                $optionalKeys = ['firstName', 'lastName', 'tp', 'year'];
+                $userKeys = array_keys($user);
+
+                $missingRequiredKeys = array_diff($requiredKeys, $userKeys);
+                $missingOptionalKeys = array_diff($optionalKeys, $userKeys);
+
+                if (!empty($missingRequiredKeys)) {
+                    $missingKeysStr = implode(', ', $missingRequiredKeys);
+                    throw new \Exception("Missing required keys in user data: $missingKeysStr");
+                }
+
+                // Si des clés optionnelles manquent, ajoutez-les avec une valeur par défaut
+                foreach ($missingOptionalKeys as $optionalKey) {
+                    $user[$optionalKey] = '';
+                }
+
+                return $user;
+            }
+        }
+
+        return null;
+    }
+
+    private function hasRequiredKeys($user, $requiredKeys)
+    {
+        foreach ($requiredKeys as $key) {
+            if (!array_key_exists($key, $user)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private function loadUsersFromJson()
+    {
+        $jsonContent = file_get_contents($this->getParameter('kernel.project_dir') . '/public/assets/json/users.json');
+        return $this->serializer->decode($jsonContent, 'json');
+    }
 }
