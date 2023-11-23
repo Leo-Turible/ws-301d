59951c2b8ab3ad44bac532fd20665be0849b2336
Leo <leo.turible@etudiant.univ-reims.fr>
2023-11-23 11:35:10 +0100

    fake reset password sending finished

--- public/assets/js/calendrier.js
+++ public/assets/js/calendrier.js
@@ -1,9 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth'
+        initialView: 'dayGridMonth'
     });
     calendar.render();
-  });
+
+    // Appelle la fonction pour charger et colorer les dates à partir du fichier JSON
+    loadAndColorDates('../json/data.json', calendar);
+});
+
+function loadAndColorDates(jsonFilePath, calendar) {
+    // Charge le fichier JSON
+    fetch(jsonFilePath)
+        .then(response => response.json())
+        .then(data => {
+            // Parcourt les événements dans le fichier JSON
+            data.events.forEach(event => {
+                // Ajoute un événement au calendrier avec la couleur spécifiée
+                calendar.addEvent({
+                    title: event.title,
+                    start: event.date,
+                    color: event.color // Assure-toi que la propriété color est présente dans ton fichier JSON
+                });
+            });
+        })
+        .catch(error => console.error('Erreur lors du chargement du fichier JSON', error));
+}
 
   
--- src/Controller/PasswordController.php
+++ src/Controller/PasswordController.php
@@ -1,18 +1,66 @@
 <?php
 
+// PasswordController.php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class PasswordController extends AbstractController
 {
-    #[Route('/password', name: 'app_password')]
-    public function index(): Response
+    private $serializer;
+
+    public function __construct(SerializerInterface $serializer)
     {
+        $this->serializer = $serializer;
+    }
+
+    #[Route('/password', name: 'app_password')]
+    public function index(Request $request, SessionInterface $session): Response
+    {
+        $email = $request->request->get('_email');
+
+        if ($request->isMethod('POST') && $email) {
+            $isValidEmail = $this->validateEmail($email);
+
+            if ($isValidEmail) {
+                // Envoyer un message de réussite
+                $this->addFlash('success', 'Un e-mail de récupération a été envoyé à votre adresse.');
+            } else {
+                // Envoyer un message d'erreur
+                $this->addFlash('error', 'Adresse e-mail non reconnue. Veuillez réessayer.');
+            }
+        }
+
         return $this->render('password/index.html.twig', [
             'controller_name' => 'PasswordController',
         ]);
+    }
+
+    private function validateEmail($email)
+    {
+        $users = $this->loadUsersFromJson();
+
+        foreach ($users as $user) {
+            if ($email === $user['email']) {
+                // E-mail trouvé, peut être considéré comme valide
+                return true;
+            }
+        }
+
+        // Aucun utilisateur avec cet e-mail trouvé
+        return false;
+    }
+
+    private function loadUsersFromJson()
+    {
+        $jsonContent = file_get_contents($this->getParameter('kernel.project_dir') . '/public/assets/json/users.json');
+        return $this->serializer->decode($jsonContent, 'json');
     }
 }
