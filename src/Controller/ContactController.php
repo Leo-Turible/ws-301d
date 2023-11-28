1e22282b26c86a7ec84e22971acd9623abefc192
Leo <leo.turible@etudiant.univ-reims.fr>
2023-11-28 09:44:36 +0100

    contact fake form made

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -1,16 +1,28 @@
 <?php
 
+// ContactController.php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        if ($request->isMethod('POST')) {
+            // Traitement du formulaire ici, par exemple, l'envoi d'un email (non inclus dans cet exemple).
+
+            // Message flash si l'email a été envoyé avec succès
+            $this->addFlash('success', 'Votre message a été envoyé avec succès. Merci !');
+
+            return $this->redirectToRoute('app_contact');
+        }
+
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
         ]);
