18994feda8821d5d7b9cece000516721c8d45a30
Leo <leo.turible@etudiant.univ-reims.fr>
2023-11-28 15:15:33 +0100

    bugs fixed

--- public/assets/js/calendrier.js
+++ public/assets/js/calendrier.js
@@ -16,6 +16,21 @@ document.addEventListener('DOMContentLoaded', function () {
                 minute: 'numeric',
                 hour12: false
             }).format(info.event.start);
+            
+            
+            var datejour = new Date();
+            var deadlineDate = new Date(info.event.start);
+            var millisecondsPerDay = 24 * 60 * 60 * 1000; // Nombre de millisecondes dans une journée
+            var daysDifference = Math.floor((deadlineDate - datejour) / millisecondsPerDay);
+            var circleCouleur; // Assurez-vous que cette variable est déclarée
+            if (daysDifference < 3) {
+                circleCouleur = 'red';
+            } else if (daysDifference < 7) {
+                circleCouleur = 'orange';
+            } else {
+                circleCouleur = 'blue';
+            }
+            
 
             infoDiv.innerHTML = `
                 <div class="event-info__content">
@@ -25,6 +40,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     <strong>Module:</strong> ${info.event.extendedProps.module} - ${info.event.extendedProps.nomCours}<br>
                     <strong>TP:</strong> ${info.event.extendedProps.tp || 'Non spécifié'}<br>
                     <strong>Type de Rendu:</strong> ${info.event.extendedProps.typeRendu || 'Non spécifié'}<br>
+                   <div id='event-info__content_cirlce'><strong>Jour Restant:  </strong> ${daysDifference}<div class="circle ${circleCouleur}"></div></div><br>
                 </div>
                 <hr>
             `;
@@ -52,7 +68,6 @@ document.addEventListener('DOMContentLoaded', function () {
                     });
                     window.location.href = '/ajout?date=' + isoString;
                 });
-                
             }
 
             infoDiv.querySelector('.event-info__close').addEventListener('click', function () {
@@ -61,13 +76,13 @@ document.addEventListener('DOMContentLoaded', function () {
                 infoDiv.classList.remove('show');
             });
         },
+
         dateClick: function (info) {
             var dayEvents = calendar.getEvents().filter(event => {
                 return event.start.getDate() === info.date.getDate();
             });
 
             dayEvents = sortByDateAndTime(dayEvents);
-
             var eventsHtml = dayEvents.map(event => {
                 var formattedDate = new Intl.DateTimeFormat('fr-FR', {
                     year: 'numeric',
@@ -86,6 +101,7 @@ document.addEventListener('DOMContentLoaded', function () {
                         <strong>Module:</strong> ${event.extendedProps.module} - ${event.extendedProps.nomCours}<br>
                         <strong>TP:</strong> ${event.extendedProps.tp || 'Non spécifié'}<br>
                         <strong>Type de Rendu:</strong> ${event.extendedProps.typeRendu || 'Non spécifié'}<br>
+                        
                     </div>
                     <hr style="margin: 1rem 0;">
                 `;
@@ -127,6 +143,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     });
 
+    
+
     infoDiv.addEventListener('click', function (event) {
         if (event.target.classList.contains('event-info__close')) {
             // Supprimer tous les éléments de la div event-info
@@ -151,56 +169,56 @@ document.addEventListener('DOMContentLoaded', function () {
         fetch('http://sae301.mmi-troyes.fr:8313/assets/json/data.json').then(response => response.json()),
         fetch('http://sae301.mmi-troyes.fr:8313/assets/json/cours.json').then(response => response.json())
     ])
-        .then(async ([jsonData, coursData]) => {
-            // ...
+    .then(async ([jsonData, coursData]) => {
+        // ...
 
-            async function colorierCases() {
-                console.log('Chargement des données JSON :', jsonData);
-            
-                const response = await fetch('/get-user-tp');
-                const data = await response.json();
-                const userTp = data.user_tp;
-            
-                const oneWeekFromNow = new Date();
-                oneWeekFromNow.setDate(new Date().getDate() + 7); // Date d'une semaine à partir d'aujourd'hui
-            
-                const threeDaysFromNow = new Date();
-                threeDaysFromNow.setDate(new Date().getDate() + 3); // Date de trois jours à partir d'aujourd'hui
-            
-                jsonData.forEach(function (event) {
-                    if (event.tp === userTp) {
-                        var coursModule = coursData.find(cours => cours.module === event.module);
-            
-                        const eventDate = new Date(event.date);
-                        const isEventPassed = eventDate < new Date(); // Vérifier si la date de l'événement est passée
-            
-                        calendar.addEvent({
-                            title: event.module,
-                            start: event.date,
-                            borderColor: isEventPassed
-                                ? 'gray'
-                                : eventDate <= threeDaysFromNow
-                                ? 'red'
-                                : eventDate <= oneWeekFromNow
-                                ? 'orange'
-                                : 'blue',
-                            classNames: isEventPassed ? ['event-passed'] : [], // Ajouter une classe si l'événement est passé
-                            extendedProps: {
-                                titre: event.titre,
-                                description: event.description,
-                                module: event.module,
-                                nomCours: coursModule ? coursModule.nomCours : 'Cours inconnu',
-                                tp: event.tp,
-                                typeRendu: event.typeRendu
-                            }
-                        });
-                    }
-                });
-            }
-            
-            colorierCases();
+        async function colorierCases() {
+            console.log('Chargement des données JSON :', jsonData);
 
-            calendar.render();
-        })
-        .catch(error => console.error('Erreur lors du chargement du fichier JSON:', error));
+            const response = await fetch('/get-user-tp');
+            const data = await response.json();
+            const userTp = data.user_tp;
+
+            const oneWeekFromNow = new Date();
+            oneWeekFromNow.setDate(new Date().getDate() + 7); // Date d'une semaine à partir d'aujourd'hui
+
+            const threeDaysFromNow = new Date();
+            threeDaysFromNow.setDate(new Date().getDate() + 3); // Date de trois jours à partir d'aujourd'hui
+
+            jsonData.forEach(function (event) {
+                if (event.tp === userTp) {
+                    var coursModule = coursData.find(cours => cours.module === event.module);
+
+                    const eventDate = new Date(event.date);
+                    const isEventPassed = eventDate < new Date(); // Vérifier si la date de l'événement est passée
+
+                    calendar.addEvent({
+                        title: event.module,
+                        start: event.date,
+                        borderColor: isEventPassed
+                            ? 'gray'
+                            : eventDate <= threeDaysFromNow
+                            ? 'red'
+                            : eventDate <= oneWeekFromNow
+                            ? 'orange'
+                            : 'blue',
+                        classNames: isEventPassed ? ['event-passed'] : [], // Ajouter une classe si l'événement est passé
+                        extendedProps: {
+                            titre: event.titre,
+                            description: event.description,
+                            module: event.module,
+                            nomCours: coursModule ? coursModule.nomCours : 'Cours inconnu',
+                            tp: event.tp,
+                            typeRendu: event.typeRendu
+                        }
+                    });
+                }
+            });
+        }
+
+        colorierCases();
+
+        calendar.render();
+    })
+    .catch(error => console.error('Erreur lors du chargement du fichier JSON:', error));
 });
